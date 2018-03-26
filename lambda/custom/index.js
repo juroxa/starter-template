8ce23bf4a4ab84a672bfd80d40402703d3343f78
Tony Du <tony@192-168-1-3.tpgi.com.au>
2018-03-26 20:47:29 +1100

    First commit

---
+++ lambda/custom/index.js
@@ -0,0 +1,36 @@
+'use strict';
+var Alexa = require("alexa-sdk");
+
+// For detailed tutorial on how to making a Alexa skill,
+// please visit us at http://alexa.design/build
+
+exports.handler = function(event, context) {
+    var alexa = Alexa.handler(event, context);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'NewSession': function () {
+        this.response.speak('Hello World!')
+        this.emit(':responseReady');
+    },
+    'SessionEndedRequest' : function() {
+        console.log('Session ended with reason: ' + this.event.request.reason);
+    },
+    'AMAZON.StopIntent' : function() {
+        this.response.speak('Bye');
+        this.emit(':responseReady');
+    },
+    'AMAZON.HelpIntent' : function() {
+        this.response.speak("You can try: 'alexa, hello world'");
+        this.emit(':responseReady');
+    },
+    'AMAZON.CancelIntent' : function() {
+        this.response.speak('Bye');
+        this.emit(':responseReady');
+    },
+    'Unhandled' : function() {
+        this.emit('AMAZON.HelpIntent')
+    }
+};
